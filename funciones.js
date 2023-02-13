cd76d3ac1676b19fb1462806bb47352cf29c5781
Ramon <ramongarciatomas@gmail.com>
2023-02-13 15:53:28 +0100

    Añadiendo operador de suma

--- funciones.js
+++ funciones.js
@@ -1,5 +1,20 @@
-function mostrarNombre(parametro){
-    var saludo = "Hola " + parametro;
+function mostrarNombre(nombre) {
+    var saludo = "Hola " + nombre.value;
     document.getElementById("resultado").innerHTML = saludo;
+}
 
+function suma(numero1, numero2) {
+    var x = document.getElementById(numero1).value;
+    var y = document.getElementById(numero2).value;
+    var sum = 0;
+    sum = Number(x) + Number(y);
+    if (isNaN(Number(x)) && isNaN(Number(y))) {
+        document.getElementById("resultado_suma").innerHTML = "Introduciste mal los números.";
+    } else if (isNaN(Number(x))) {
+        document.getElementById("resultado_suma").innerHTML = "Introduciste mal el primer número.";
+    } else if (isNaN(Number(y))) {
+        document.getElementById("resultado_suma").innerHTML = "Introduciste mal el segundo número.";
+    } else {
+        document.getElementById("resultado_suma").innerHTML = sum;
+    }
 }
